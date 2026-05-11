d4423781e77dc66ff3ebc121a4f7b78f2201c880
Jason Wu <24651@burnside.school.nz>
2026-05-12 11:24:25 +1200

    Added structural comments and docstring

--- parking_manager.py
+++ parking_manager.py
@@ -1,2 +1,19 @@
-# quick sanity check
-print("Hello World!")
+#!/bin/python3
+
+# docstring
+'''
+parking_manager.py - A Management Interface for the Parking Enforcer Database Application.
+
+Copyright (c) 2026 Jason Wu. All rights reserved.
+'''
+
+# imports
+import sqlite3
+
+# constants and variables
+DATABASE = "parking.db"
+
+# functions
+
+
+# main
